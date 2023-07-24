24177ab06183c4bed13a0090a58fc3fd0e2103b8
Albert Brown <albertbrown@Bobingy.local>
2023-07-24 16:51:22 -0700

    some test boilerplate

---
+++ src/test/utils/errors.ts
@@ -0,0 +1,10 @@
+/**
+ * @notice Contains all error strings used in the contracts.
+ * Should be exported in package
+ */
+export const ERROR_STRINGS = {
+    CurrencyGovernance: {
+        EXAMPLE: 'error string',
+    }
+  }
+  
