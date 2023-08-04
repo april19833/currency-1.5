dfe1ab3522cb3dbf9b086f6c54330aa82a925250
Albert Brown <albertbrown@Bobingy.local>
2023-08-04 13:44:52 -0700

    ported over all error strings

--- test/utils/errors.ts
+++ test/utils/errors.ts
@@ -5,6 +5,12 @@
 export const ERRORS = {
     CurrencyGovernance: {
         TRUSTEE_ONLY: 'TrusteeOnlyFunction',
+        REQUIRE_NON_ZERO_ADDRESS: 'NonZeroTrustedNodesAddr',
+        WRONG_STAGE: 'WrongStage',
+        // two args
+        CYCLE_INACTIVE: 'CycleInactive',
+        // two args
+        CYCLE_INCOMPLETE: 'CycleIncomplete',
     },
     Policed: {
         POLICY_ONLY: 'PolicyOnlyFunction',
