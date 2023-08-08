481a6ae6b4f18bab1a14bba264127fafbcd79b33
Albert Brown <albertbrown@Bobingy.local>
2023-08-08 13:39:50 -0700

    explicit import dependencies doesn't rely on import chaining

--- contracts/governance/monetary/CurrencyGovernance.sol
+++ contracts/governance/monetary/CurrencyGovernance.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.0;
 
 import "./TrustedNodes.sol";
+import "../../utils/TimeUtils.sol";
+import "../../policy/Policed.sol";
 
 /** @title Trustee monetary policy decision process
  *
