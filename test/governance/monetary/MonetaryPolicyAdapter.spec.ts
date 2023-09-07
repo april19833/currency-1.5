e0367bf200752c180d72df08978ba25df28ab09c
Albert Brown <albertbrown@Bobingy.local>
2023-09-07 13:41:47 -0700

    extensive tests for happy path tests for all the reverts we wish to support mock contracts couldn't take the heat with these reverting calldatas

--- test/governance/monetary/MonetaryPolicyAdapter.spec.ts
+++ test/governance/monetary/MonetaryPolicyAdapter.spec.ts
@@ -21,8 +21,16 @@ const alwaysRevertSig = ethers.utils.solidityKeccak256(['string'], ['alwaysRever
 // proposalId doesn't matter but we want to check that it's passed through correctly
 const proposalId = ethers.utils.hexlify(ethers.utils.randomBytes(32))
 const PLACEHOLDER_ADDRESS1 = '0x1111111111111111111111111111111111111111'
-const PLACEHOLDER_BYTES32 = ethers.utils.hexlify(ethers.utils.randomBytes(32))
-const PLACEHOLDER_UINT256 = 999
+const PLACEHOLDER_ADDRESS2 = '0x2222222222222222222222222222222222222222'
+const PLACEHOLDER_BYTES32_1 = ethers.utils.hexlify(ethers.utils.randomBytes(32))
+const PLACEHOLDER_BYTES32_2 = '0x' + '00'.repeat(32)
+const PLACEHOLDER_BYTES32_3 = ethers.utils.hexlify(ethers.utils.randomBytes(32))
+const PLACEHOLDER_UINT256_1 = 999
+const PLACEHOLDER_UINT256_2 = 1
+const PLACEHOLDER_UINT256_3 = 0
+const REVERTING_UINT156_1 = 1000
+const REVERTING_UINT156_2 = 1001
+const REVERTING_UINT156_3 = 1001932810298400
 
 describe.only('MonetaryPolicyAdapter', () => {
     let alice: SignerWithAddress
@@ -35,10 +43,10 @@ describe.only('MonetaryPolicyAdapter', () => {
     })
   
     let Enacter: MonetaryPolicyAdapter
-    let DummyLeverMockFactory: MockContractFactory<DummyLever__factory>
-    let DummyLever1: MockContract<DummyLever>
-    let DummyLever2: MockContract<DummyLever>
-    let DummyLever3: MockContract<DummyLever>
+    let DummyLeverFactory: DummyLever__factory
+    let DummyLever1: DummyLever
+    let DummyLever2: DummyLever
+    let DummyLever3: DummyLever
     let Fake__CurrencyGovernance: FakeContract<CurrencyGovernance>
     let Fake__Policy: FakeContract<Policy>
   
@@ -58,51 +66,49 @@ describe.only('MonetaryPolicyAdapter', () => {
             .connect(policyImpersonater)
             .deploy(Fake__Policy.address, Fake__CurrencyGovernance.address)
 
-        DummyLeverMockFactory = await smock.mock<DummyLever__factory>('DummyLever')
-
-        DummyLever1 = await DummyLeverMockFactory.deploy()
-        DummyLever2 = await DummyLeverMockFactory.deploy()
-        DummyLever3 = await DummyLeverMockFactory.deploy()
+        DummyLeverFactory = new DummyLever__factory()
+        DummyLever1 = await DummyLeverFactory.connect(policyImpersonater).deploy()
+        DummyLever2 = await DummyLeverFactory.connect(policyImpersonater).deploy()
+        DummyLever3 = await DummyLeverFactory.connect(policyImpersonater).deploy()
     })
 
     // TODO
     describe('roles', () => {})
 
     describe('dummy lever signatures', () => {
-        it('check execute signature', async () => {
-            const compiledSig = DummyLeverMockFactory.interface.getSighash('execute')
+        it.only('check execute signature', async () => {
+            const compiledSig = DummyLeverFactory.interface.getSighash('execute')
             const contractSig = await DummyLever1.executeFunctionSignature()
             expect(executeSig).to.eql(compiledSig).to.eql(contractSig)
         })
 
         it('check alwaysPass signature', async () => {
-            const compiledSig = DummyLeverMockFactory.interface.getSighash('alwaysPass')
+            const compiledSig = DummyLeverFactory.interface.getSighash('alwaysPass')
             const contractSig = await DummyLever1.alwaysPassFunctionSignature()
             expect(alwaysPassSig).to.eql(compiledSig).to.eql(contractSig)
         })
 
         it('check datalessPasser signature', async () => {
-            const compiledSig = DummyLeverMockFactory.interface.getSighash('datalessPasser')
+            const compiledSig = DummyLeverFactory.interface.getSighash('datalessPasser')
             const contractSig = await DummyLever1.datalessPasserFunctionSignature()
             expect(datalessPasserSig).to.eql(compiledSig).to.eql(contractSig)
         })
 
         it('check alwaysRevert signature', async () => {
-            const compiledSig = DummyLeverMockFactory.interface.getSighash('alwaysRevert')
+            const compiledSig = DummyLeverFactory.interface.getSighash('alwaysRevert')
             const contractSig = await DummyLever1.alwaysRevertFunctionSignature()
             expect(alwaysRevertSig).to.eql(compiledSig).to.eql(contractSig)
         })
     })
 
     describe('enact', () => {
-        
-
-        describe.only('happy path', () => {
+        describe('happy path', () => {
             it('can enact the simplest policy', async () => {
                 const targets = [DummyLever1.address]
                 const signatures = [alwaysPassSig]
-                const calldatas = [PLACEHOLDER_BYTES32]
+                const calldatas = [PLACEHOLDER_BYTES32_1]
                 await Enacter.connect(cgImpersonater).enact(proposalId, targets, signatures, calldatas)
+                
                 const count = await DummyLever1.executeMarker()
                 expect(count.eq(3)).to.be.true
             })
@@ -112,6 +118,7 @@ describe.only('MonetaryPolicyAdapter', () => {
                 const signatures = [datalessPasserSig]
                 const calldatas = ['0x']
                 await Enacter.connect(cgImpersonater).enact(proposalId, targets, signatures, calldatas)
+                
                 const count = await DummyLever1.executeMarker()
                 expect(count.eq(5)).to.be.true
             })
@@ -119,33 +126,152 @@ describe.only('MonetaryPolicyAdapter', () => {
             it('can enact a policy that passes empty signature', async () => {
                 const targets = [DummyLever1.address]
                 const signatures = ['0x00000000'] // bytes4(0) signifies the fallback function
-                const calldatas = [PLACEHOLDER_BYTES32]
+                const calldatas = [PLACEHOLDER_BYTES32_3]
                 await Enacter.connect(cgImpersonater).enact(proposalId, targets, signatures, calldatas)
+                
                 const count = await DummyLever1.executeMarker()
                 expect(count.eq(11)).to.be.true
-            })
-
-            it('can enact a policy that reverts', async () => {
-                const targets = [DummyLever1.address]
-                const signatures = [alwaysRevertSig]
-                const calldatas = [PLACEHOLDER_BYTES32]
-                await Enacter.connect(cgImpersonater).enact(proposalId, targets, signatures, calldatas)
-                const count = await DummyLever1.executeMarker()
-                expect(count.eq(0)).to.be.true
             })
 
             it('can enact the most complicated example calldata', async () => {
                 const targets = [DummyLever1.address]
                 const signatures = [executeSig]
-                const calldatas = [ethers.utils.defaultAbiCoder.encode(['uint256','address','bytes32'], [PLACEHOLDER_UINT256, PLACEHOLDER_ADDRESS1, PLACEHOLDER_BYTES32])]
+                const calldatas = [ethers.utils.defaultAbiCoder.encode(['uint256','address','bytes32'], [PLACEHOLDER_UINT256_1, PLACEHOLDER_ADDRESS1, PLACEHOLDER_BYTES32_1])]
                 await Enacter.connect(cgImpersonater).enact(proposalId, targets, signatures, calldatas)
+                
                 const count = await DummyLever1.executeMarker()
                 expect(count.eq(2)).to.be.true
             })
+
+            it('can enact a policy that targets multiple levers', async () => {
+                const targets = [DummyLever1.address, DummyLever2.address, DummyLever3.address]
+                const signatures = [executeSig, alwaysPassSig, executeSig]
+                const calldata1 = ethers.utils.defaultAbiCoder.encode(['uint256','address','bytes32'], [PLACEHOLDER_UINT256_2, PLACEHOLDER_ADDRESS2, PLACEHOLDER_BYTES32_1])
+                const calldata2 = PLACEHOLDER_BYTES32_3
+                const calldata3 = ethers.utils.defaultAbiCoder.encode(['uint256','address','bytes32'], [PLACEHOLDER_UINT256_3, PLACEHOLDER_ADDRESS1, PLACEHOLDER_BYTES32_2])
+                const calldatas = [calldata1, calldata2, calldata3]
+                await Enacter.connect(cgImpersonater).enact(proposalId, targets, signatures, calldatas)
+                
+                const count1 = await DummyLever1.executeMarker()
+                expect(count1.eq(2)).to.be.true
+                const count2 = await DummyLever2.executeMarker()
+                expect(count2.eq(3)).to.be.true
+                const count3 = await DummyLever3.executeMarker()
+                expect(count3.eq(2)).to.be.true
+            })
+
+            it('can enact a policy that targets the same lever multiple times', async () => {
+                const targets = [DummyLever1.address, DummyLever1.address, DummyLever3.address]
+                const signatures = [executeSig, alwaysPassSig, executeSig]
+                const calldata1 = ethers.utils.defaultAbiCoder.encode(['uint256','address','bytes32'], [PLACEHOLDER_UINT256_2, PLACEHOLDER_ADDRESS2, PLACEHOLDER_BYTES32_1])
+                const calldata2 = PLACEHOLDER_BYTES32_3
+                const calldata3 = ethers.utils.defaultAbiCoder.encode(['uint256','address','bytes32'], [PLACEHOLDER_UINT256_3, PLACEHOLDER_ADDRESS1, PLACEHOLDER_BYTES32_2])
+                const calldatas = [calldata1, calldata2, calldata3]
+                await Enacter.connect(cgImpersonater).enact(proposalId, targets, signatures, calldatas)
+                
+                const count1 = await DummyLever1.executeMarker()
+                expect(count1.eq(5)).to.be.true
+                const count2 = await DummyLever2.executeMarker()
+                expect(count2.eq(0)).to.be.true
+                const count3 = await DummyLever3.executeMarker()
+                expect(count3.eq(2)).to.be.true
+            })
+
+            it('emits the EnactedMonetaryPolicy event', async () => {
+                const targets = [DummyLever1.address, DummyLever2.address, DummyLever3.address]
+                const signatures = [executeSig, alwaysPassSig, executeSig]
+                const calldata1 = ethers.utils.defaultAbiCoder.encode(['uint256','address','bytes32'], [PLACEHOLDER_UINT256_2, PLACEHOLDER_ADDRESS2, PLACEHOLDER_BYTES32_1])
+                const calldata2 = PLACEHOLDER_BYTES32_3
+                const calldata3 = ethers.utils.defaultAbiCoder.encode(['uint256','address','bytes32'], [PLACEHOLDER_UINT256_3, PLACEHOLDER_ADDRESS1, PLACEHOLDER_BYTES32_2])
+                const calldatas = [calldata1, calldata2, calldata3]
+                await expect(Enacter.connect(cgImpersonater).enact(proposalId, targets, signatures, calldatas))
+                    .to.emit(Enacter, 'EnactedMonetaryPolicy')
+                    .withArgs(proposalId, Fake__CurrencyGovernance.address, [true, true, true])
+                
+
+            })
         })
 
-        describe('reverts', () => {
+        // despite including reverts, these are still normal behavior tests as the revert safety is part of the normal functionality
+        describe.only('reverts', () => {
+            it('can enact a policy that reverts', async () => {
+                const targets = [DummyLever1.address]
+                const signatures = [alwaysRevertSig]
+                const calldatas = [PLACEHOLDER_BYTES32_2]
+                await Enacter.connect(cgImpersonater).enact(proposalId, targets, signatures, calldatas)
+                
+                const count = await DummyLever1.executeMarker()
+                expect(count.eq(0)).to.be.true
+            })
 
+            it('can enact a policy that reverts due to input validation', async () => {
+                const targets = [DummyLever1.address]
+                const signatures = [executeSig]
+                // the uint256 input is too high and causes a revert
+                const calldatas = [ethers.utils.defaultAbiCoder.encode(['uint256','address','bytes32'], [REVERTING_UINT156_1, PLACEHOLDER_ADDRESS1, PLACEHOLDER_BYTES32_2])]
+                await Enacter.connect(cgImpersonater).enact(proposalId, targets, signatures, calldatas)
+                
+                const count = await DummyLever1.executeMarker()
+                expect(count.eq(0)).to.be.true
+            })
+
+            it('can enact a policy that reverts due to bad input data', async () => {
+                const targets = [DummyLever1.address]
+                const signatures = [executeSig]
+                // calldata is garbage and doesn't match the function
+                // hardhat dies when trying to get the return type here, but it doesn't revert
+                const calldatas = [PLACEHOLDER_BYTES32_1]
+                await Enacter.connect(cgImpersonater).enact(proposalId, targets, signatures, calldatas)
+                
+                const count = await DummyLever1.executeMarker()
+                expect(count.eq(0)).to.be.true
+            })
+
+            it('can enact a policy that reverts due to mistyped data', async () => {
+                const targets = [DummyLever1.address]
+                const signatures = [executeSig]
+                // calldata is mistyped
+                const calldatas = [ethers.utils.defaultAbiCoder.encode(['bytes32','bytes32','bytes32'], [PLACEHOLDER_BYTES32_1, PLACEHOLDER_BYTES32_2, PLACEHOLDER_BYTES32_3])]
+                await Enacter.connect(cgImpersonater).enact(proposalId, targets, signatures, calldatas)
+                
+                const count = await DummyLever1.executeMarker()
+                expect(count.eq(0)).to.be.true
+            })
+
+            it('reverts don\'t block other txs in the policy', async () => {
+                const targets = [DummyLever1.address, DummyLever2.address, DummyLever3.address]
+                const signatures = [executeSig, alwaysPassSig, executeSig]
+                const calldata1 = ethers.utils.defaultAbiCoder.encode(['uint256','address','bytes32'], [REVERTING_UINT156_2, PLACEHOLDER_ADDRESS2, PLACEHOLDER_BYTES32_1])
+                const calldata2 = PLACEHOLDER_BYTES32_3
+                const calldata3 = ethers.utils.defaultAbiCoder.encode(['uint256','address','bytes32'], [REVERTING_UINT156_3, PLACEHOLDER_ADDRESS1, PLACEHOLDER_BYTES32_2])
+                const calldatas = [calldata1, calldata2, calldata3]
+                await Enacter.connect(cgImpersonater).enact(proposalId, targets, signatures, calldatas)
+                
+                const count1 = await DummyLever1.executeMarker()
+                expect(count1.eq(0)).to.be.true
+                const count2 = await DummyLever2.executeMarker()
+                expect(count2.eq(3)).to.be.true
+                const count3 = await DummyLever3.executeMarker()
+                expect(count3.eq(0)).to.be.true
+            })
+
+            it('reverts are reflected in the event', async () => {
+                const targets = [DummyLever1.address, DummyLever2.address, DummyLever3.address]
+                const signatures = [executeSig, alwaysPassSig, alwaysRevertSig]
+                const calldata1 = ethers.utils.defaultAbiCoder.encode(['uint256','address','bytes32'], [REVERTING_UINT156_2, PLACEHOLDER_ADDRESS2, PLACEHOLDER_BYTES32_1])
+                const calldata2 = PLACEHOLDER_BYTES32_2
+                const calldata3 = PLACEHOLDER_BYTES32_3
+                const calldatas = [calldata1, calldata2, calldata3]
+                await expect(Enacter.connect(cgImpersonater).enact(proposalId, targets, signatures, calldatas))
+                    .to.emit(Enacter, 'EnactedMonetaryPolicy')
+                    .withArgs(proposalId, Fake__CurrencyGovernance.address, [false, true, false])
+            })
+
+            describe('exploits', () => {
+                it('revert due to out of gas', async () => {
+
+                }) 
+            })
         })
     })
 })
