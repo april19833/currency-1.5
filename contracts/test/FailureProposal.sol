a77b75de016b585f4364430a076d2adce9ac258c
Albert Brown <albertbrown@Bobingy.local>
2023-10-27 00:04:32 -0700

    failing proposal for test

---
+++ contracts/test/FailureProposal.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../governance/community/proposals/Proposal.sol";
+import "../policy/Policy.sol";
+
+/** @title SampleProposal
+ *
+ * A proposal used for testing proposal adoption.
+ */
+contract SampleProposal is Policy, Proposal {
+    /** Apologize in case of failure
+     */
+    error ImSorry();
+
+    /** The name of the proposal.
+     */
+    function name() public pure override returns (string memory) {
+        return "Reverter";
+    }
+
+    /** A description of what the proposal does.
+     */
+    function description() public pure override returns (string memory) {
+        return "Built to fail!";
+    }
+
+    /** A URL for more information.
+     */
+    function url() public pure override returns (string memory) {
+        return "http://something";
+    }
+
+    /** Enact the proposal.
+     */
+    function enacted(address _self) public override {
+        revert ImSorry();
+    }
+}
