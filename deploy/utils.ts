d5a6455e3094a8b191bb3558102f97b44a8a0623
Albert Brown <albertbrown@Bobingy.local>
2023-11-01 12:07:32 -0700

    new util for contract deployment

---
+++ deploy/utils.ts
@@ -0,0 +1,32 @@
+import { Contract, ContractFactory, Signer } from 'ethers'
+import {
+    ForwardProxy__factory,
+} from '../typechain-types'
+
+/**
+ * Deploy a contract with the given factory from a certain address
+ * Will be deployed by the given deployer address with the given params
+ */
+export async function deploy<F extends ContractFactory>(
+  from: Signer,
+  factoryType: { new(from: Signer): F ;},
+  params?: any[]
+): Promise<Contract> {
+  const factory = new factoryType(from)
+  return params ? factory.deploy(...params) : factory.deploy()
+}
+
+/**
+ * Deploy a proxied contract with the given factory from a certain address
+ * Will be deployed with the given params
+ */
+export async function deployProxy<F extends ContractFactory>(
+  from: Signer,
+  factoryType: { new(from: Signer): F ;},
+  params?: any[]
+): Promise<Contract> {
+  const factory = new factoryType(from)
+  const base = params ? await factory.deploy(...params) : await factory.deploy()
+  const proxy = await (new ForwardProxy__factory(from)).deploy(base.address)
+  return factory.attach(proxy.address)
+}
