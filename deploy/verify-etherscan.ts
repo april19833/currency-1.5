ca0fe857f81bd6d6804d8e6ee0025a57320b24fc
Albert Brown <albertbrown@Bobingy.local>
2023-11-22 14:45:26 -0800

    script for verifying deploys on etherscan still can't parse the proxy but that's ok imo

---
+++ deploy/verify-etherscan.ts
@@ -0,0 +1,36 @@
+import { run } from 'hardhat'
+
+const buildNumber = '1700691171508'
+
+const addresses = require(`./deployments/${buildNumber}/addresses.json`)
+const baseImplementationAddresses = require(`./deployments/${buildNumber}/baseImplementationAddresses.json`)
+const baseDeployParams = require(`./deployments/${buildNumber}/baseDeployParams.json`)
+const monetaryDeployParams = require(`./deployments/${buildNumber}/monetaryDeployParams.json`)
+const communityDeployParams = require(`./deployments/${buildNumber}/communityDeployParams.json`)
+
+const deployParams = {
+    ...baseDeployParams,
+    ...monetaryDeployParams,
+    ...communityDeployParams,
+}
+
+async function main() {
+  addresses.policy = baseImplementationAddresses.policyImplementation
+  addresses.eco = baseImplementationAddresses.ecoImplementation
+  addresses.ecox = baseImplementationAddresses.ecoxImplementation
+  addresses.ecoXStaking = baseImplementationAddresses.ecoXStakingImplementation
+
+  const contractNames = Object.keys(addresses)
+  
+  for(const contractName of contractNames) {
+    await run('verify:verify', {
+        address: addresses[contractName],
+        constructorArguments: deployParams[`${contractName}Params`],
+    })
+  }
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
